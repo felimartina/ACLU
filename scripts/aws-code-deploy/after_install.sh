9a6d0f35e5d929fcf7b57b4af5e4a528b4e3e480
Pipe Martina <felimartina@outlook.com>
2017-10-18 13:39:03 -1000

    added sudo to all docker commands

--- scripts/aws-code-deploy/after_install.sh
+++ scripts/aws-code-deploy/after_install.sh
@@ -4,10 +4,10 @@ set -e # stop bash execution on on first error
 
 # Docker compose
 cd /var/project-aclu/
-docker-compose down
-docker-compose up
+sudo docker-compose down
+sudo docker-compose up
 
 # Create the spatial index
 # TODO - Not sure if it is ok to run this on every deploy...is it idempotent?
 # TODO2 - What about import scripts?
-docker exec $(docker ps -aqf "name=aclu-db") mongo aclu --eval "db.features.ensureIndex({'geojson.geometry': '2dsphere'})"
+sudo docker exec $(docker ps -aqf "name=aclu-db") mongo aclu --eval "db.features.ensureIndex({'geojson.geometry': '2dsphere'})"
